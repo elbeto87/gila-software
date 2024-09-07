af4d65c0cb8aaaf98e7778486d15bee3c61e6a47
Gresia, Ezequiel <Ezequiel.Gresia@gilbarco.com>
2024-09-07 15:45:48 -0300

    refactor

--- app/subscription/channels.py
+++ app/subscription/channels.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+
 from app.subscription.enums import ChannelEnum
 from app.logger import logger
 
@@ -7,5 +9,33 @@ class Notification:
     def __init__(self, type_of_notification: ChannelEnum):
         self.type_of_notification = type_of_notification
 
+    @abstractmethod
     def send_message(self, user: str, message: str):
-        logger.info(f"Sending {self.type_of_notification} message '{message}' to user: {user}")
+        raise NotImplementedError()
+
+
+class PushNotification(Notification):
+
+    def __init__(self):
+        super().__init__(ChannelEnum.PUSH_NOTIFICATION)
+
+    def send_message(self, user: str, message: str):
+        logger.info(f"Sending push notification to {user}: {message}")
+
+
+class EmailNotification(Notification):
+
+    def __init__(self):
+        super().__init__(ChannelEnum.EMAIL)
+
+    def send_message(self, user: str, message: str):
+        logger.info(f"Sending email to {user}: {message}")
+
+
+class SMSNotification(Notification):
+
+    def __init__(self):
+        super().__init__(ChannelEnum.SMS)
+
+    def send_message(self, user: str, message: str):
+        logger.info(f"Sending SMS to {user}: {message}")
